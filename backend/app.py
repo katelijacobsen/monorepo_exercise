bfe4194f66bb1d03d91be8c60f1d6b5edb1219d9
katelijacobsen <katjamaehleke98@gmail.com>
2026-03-23 08:43:48 +0100

    docker compose up --build

---
+++ backend/app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request, jsonify, session, redirect
+import config
+import uuid
+import time
+from flask_session import Session
+from werkzeug.security import generate_password_hash
+from werkzeug.security import check_password_hash
+from flask_cors import CORS 
+
+from icecream import ic
+
+
+ic.configureOutput(prefix=f'______________ | ', includeContext=True)
+
+app = Flask(__name__)
+
+app.config['SESSION_TYPE'] = 'filesystem'
+Session(app)
+
+CORS(app, origins=["http://localhost:3000"])
+
+##############################
+@app.route("/")
+def show_index():
+    return render_template("index.html")
--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const nextConfig : NextConfig = {
+  reactStrictMode: true,
+  async rewrites() {
+    return [
+      { source: "/api/:path*", destination: `${process.env.NEXT_PUBLIC_API_URL}/api/:path*` }
+    ]
+  }
+}
 
-export default nextConfig;
+module.exports = nextConfig;
