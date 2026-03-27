9a697ce50728cb31c65f78e0ead6dff1c694a1a0
katelijacobsen <145345930+katelijacobsen@users.noreply.github.com>
2026-03-27 09:01:29 +0100

    push email function

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
+// Get the data
 export const getName = async (url: string) => {
   const res = await axios.get(url);
-  console.log("response data:", res.data);
   return res.data;
 };
+
--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -4,7 +4,7 @@ const nextConfig : NextConfig = {
   reactStrictMode: true,       // ← keep this from your config
   async rewrites() {
     return [
-      { source: "/:path*", destination: "http://localhost:80/:path*" }
+      { source: "/:path*", destination: "http://localhost:80/:path*" } //use this port. Its says in Dockerfile where you run Flask
     ]
   }
 }
